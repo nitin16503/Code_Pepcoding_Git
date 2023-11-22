1aa1e35524cc0da6a4e7abf0c80ef8c7ff63c236
Nitin Mehta <115355852+nitin16503@users.noreply.github.com>
2023-11-22 09:44:07 +0530

    Update b_Level_1_Pattern.java

--- b_Level_1_Pattern.java
+++ b_Level_1_Pattern.java
@@ -78,30 +78,30 @@ public class b_Level_1_Pattern {
 //		}
 		
 //#21                    STAR		                         //* *   * *
-//		int n=hemlo.nextInt();                               //*       *
-//		int star=n+1;                                        //* *   * *
-//		int space=1;
-//		for(int i=1; i<=n;i++) {
-//			//System.out.print(space+" "+star);
-//			for (int j=1;j<=star/2;j++) {
-//				
-//				System.out.print("* ");
-//			}
-//			for(int k=1;k<=space;k++) {
-//				System.out.print("  ");
-//			}
-//			for(int l=star/2+1;l<=star;l++) {
-//				System.out.print("* ");
-//			}
-//			if(i<=n/2) {
-//				star-=2;
-//				space+=2;
-//			}else {
-//				star+=2;
-//				space-=2;
-//			}
-//			System.out.println();
-//		}
+		int n=hemlo.nextInt();                               //*       *
+		int star=n+1;                                        //* *   * *
+		int space=1;
+		for(int i=1; i<=n;i++) {
+			//System.out.print(space+" "+star);
+			for (int j=1;j<=star/2;j++) {
+				
+				System.out.print("* ");
+			}
+			for(int k=1;k<=space;k++) {
+				System.out.print("  ");
+			}
+			for(int l=star/2+1;l<=star;l++) {
+				System.out.print("* ");
+			}
+			if(i<=n/2) {
+				star-=2;
+				space+=2;
+			}else {
+				star+=2;
+				space-=2;
+			}
+			System.out.println();
+		}
 		
 //#22	               STAR
 //		int n=hemlo.nextInt();                      //*
